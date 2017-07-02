d24eaa1a4d823155be3d7c504a120bd27ea58384
Justas Lavišius <bucaneer@gmail.com>
2017-07-02 16:40:53 +0300

    update copyright year

--- binauralconv.py
+++ binauralconv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Copyright (c) 2016 Justas Lavišius <bucaneer@gmail.com>
+Copyright (c) 2016, 2017 Justas Lavišius <bucaneer@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
